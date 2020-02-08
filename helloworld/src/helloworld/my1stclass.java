afd277b5a69932ccad1e18e82a12163968ac9e3f
asus <asus@DESKTOP-QUUGNC3>
2020-02-08 03:05:20 +0200

    initial commit of project

---
+++ helloworld/src/helloworld/my1stclass.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class my1stclass 
+{		 
+	    /**
+	     * @param args
+	     */
+	    public static void main(String[] args)
+	    {
+	        System.out.println("Hello World");
+	    }
+	 
+}
